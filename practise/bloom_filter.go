e6c5ed303d099c39a553a34b55d7a03081641175
wuhui.zhang <wuhui.zhang@gloryholiday.com>
2020-06-02 14:39:05 +0800

    bloom filter

--- practise/bloom_filter.go
+++ practise/bloom_filter.go
@@ -1,11 +1,56 @@
 package practise
 
+import "math"
+
 type BloomFilter interface {
-	Put(data []byte)
-	IsExists(data []byte) bool
+	Set(data []byte)
+	Get(data []byte) bool
 }
 
 type bloomFilter struct {
-	size int16
-	hasher 
+	m int //size = - n log p / (log2 )^2
+	bitset []bool
+	k int // k = ln2 * m /n
+	faultRate float64
+}
+
+func NewBloomFilter(faultRate float64, size int) *bloomFilter {
+	m := -math.Log(faultRate) * float64(size) / math.Ln2*math.Ln2
+	k := math.Ln2 * m / float64(size)
+	return &bloomFilter{
+		m: int(m),
+		k: int(k),
+		bitset: make([]bool, int(m)),
+		faultRate: faultRate,
+	}
+}
+
+func (filter *bloomFilter) Set(data []byte) {
+	for i := 0;i < filter.k;i++ {
+		hash := filter.position(data, i)
+		filter.bitset[hash] = true
+	}
+}
+
+func (filter *bloomFilter) Get(data []byte) bool  {
+	for i := 0;i < filter.k;i++ {
+		hash := filter.position(data, i)
+		if !filter.bitset[hash] {
+			return false
+		}
+	}
+	return true
+}
+
+func (filter *bloomFilter) position(data []byte, i int) int {
+	hs := filter.hash(data, i)
+	return int(math.Abs(float64(hs % (filter.m - 1))))
+}
+
+func (filter *bloomFilter) hash(data []byte, i int) int {
+	hash := 17
+	for _, d := range data {
+		hash = hash * 31 + int(d) + i
+	}
+	return hash
 }
--- practise/bloom_filter_test.go
+++ practise/bloom_filter_test.go
@@ -9,3 +9,21 @@
 // entered into with YueTu Ltd.
 
 package practise
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBloomFilter(t *testing.T) {
+	filter := NewBloomFilter(0.001, 1000)
+	filter.Set([]byte("abcd"))
+	filter.Set([]byte("aaaa"))
+	filter.Set([]byte("bbdcd"))
+
+	assert.Equal(t, filter.Get([]byte("abcd")), true)
+	assert.Equal(t, filter.Get([]byte("bbdcd")), true)
+
+	assert.Equal(t, filter.Get([]byte("abc")), false)
+}
