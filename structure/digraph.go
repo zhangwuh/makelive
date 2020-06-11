4c0178a33d2b26315dc20ed768bca1f307588226
wuhui.zhang <wuhui.zhang@gloryholiday.com>
2020-06-11 15:37:35 +0800

    strong connected check

--- structure/digraph.go
+++ structure/digraph.go
@@ -1,5 +1,7 @@
 package structure
 
+import "mklive.zhangwuh.com/utils"
+
 type Digraph interface {
 	Graph
 	Reverse() Digraph
@@ -64,6 +66,7 @@ func NewDirectedDfs(g Digraph, sources []int) *DirectedDfs {
 	for _, s := range sources {
 		d.dfs(s)
 	}
+
 	return d
 }
 
@@ -71,7 +74,6 @@ func (dfs *DirectedDfs) dfs(s int) {
 	dfs.marked[s] = true
 	for _, n := range dfs.g.Adj(s) {
 		if !dfs.isMarked(n.(int)) {
-			dfs.marked[n.(int)] = true
 			dfs.dfs(n.(int))
 		}
 	}
@@ -79,4 +81,219 @@ func (dfs *DirectedDfs) dfs(s int) {
 
 func (dfs *DirectedDfs) isMarked(i int) bool {
 	return dfs.marked[i]
+}
+
+type DirectedCycleDetector struct {
+	g       Digraph
+	stack   Stack
+	edgeTo  []int
+	marked  []bool
+	onstack []bool
+}
+
+func NewDirectedCycleDetector(g Digraph) *DirectedCycleDetector {
+	dc := &DirectedCycleDetector{
+		g:       g,
+		stack:   NewLinkedStack(nil),
+		marked:  make([]bool, g.V()),
+		onstack: make([]bool, g.V()),
+		edgeTo:  make([]int, g.V()),
+	}
+	for i := 0; i < g.V(); i++ {
+		dc.dfs(i)
+	}
+	return dc
+}
+
+func (dc *DirectedCycleDetector) HasCycle() bool {
+	return !dc.stack.IsEmpty()
+}
+func (dc *DirectedCycleDetector) Cycle() []int {
+	var c []int
+	for !dc.stack.IsEmpty() {
+		c = append(c, dc.stack.Pop().(int))
+	}
+	return c
+}
+
+func (dfs *DirectedCycleDetector) dfs(s int) {
+	dfs.marked[s] = true
+	dfs.onstack[s] = true
+	for _, n := range dfs.g.Adj(s) {
+		current := n.(int)
+		dfs.edgeTo[current] = s
+		if dfs.HasCycle() {
+			break
+		}
+		if !dfs.isMarked(current) {
+			dfs.dfs(current)
+		} else {
+			if dfs.onstack[current] {
+				for x := s; x != current; x = dfs.edgeTo[x] {
+					dfs.stack.Push(x)
+				}
+				dfs.stack.Push(current)
+				dfs.stack.Push(s)
+			}
+		}
+
+	}
+	dfs.onstack[s] = false
+}
+
+func (dfs *DirectedCycleDetector) isMarked(i int) bool {
+	return dfs.marked[i]
+}
+
+type DepthFirstOrder struct {
+	g           Digraph
+	pre         []int
+	post        []int
+	reversePost Stack
+	marked      []bool
+}
+
+func NewDepthFirstOrder(g Digraph) *DepthFirstOrder {
+	do := &DepthFirstOrder{
+		g:           g,
+		marked:      make([]bool, g.V()),
+		reversePost: NewLinkedStack(nil),
+	}
+
+	for v := 0; v < g.V(); v++ {
+		do.dfs(v)
+	}
+
+	return do
+}
+
+func (dfo *DepthFirstOrder) dfs(v int) {
+	if dfo.marked[v] {
+		return
+	}
+	dfo.marked[v] = true
+	dfo.pre = append(dfo.pre, v)
+	for _, w := range dfo.g.Adj(v) {
+		current := w.(int)
+		if !dfo.marked[current] {
+			dfo.dfs(current)
+		}
+	}
+	dfo.post = append(dfo.post, v)
+	dfo.reversePost.Push(v)
+}
+
+func (dfo *DepthFirstOrder) reversePostQueue() []int {
+	var q []int
+	for !dfo.reversePost.IsEmpty() {
+		q = append(q, dfo.reversePost.Pop().(int))
+	}
+	return q
+}
+
+type Topological interface {
+	isDAG() bool
+	order() []int
+}
+
+type topologicalSorter struct {
+	g     Digraph
+	stack Stack
+}
+
+func NewTopologicalSorter(g Digraph) *topologicalSorter {
+	return &topologicalSorter{
+		g:     g,
+		stack: NewLinkedStack(nil),
+	}
+}
+
+func (ts *topologicalSorter) isDAG() bool {
+	dd := NewDirectedCycleDetector(ts.g)
+	return !dd.HasCycle()
+}
+
+func (ts *topologicalSorter) order() []int {
+	if !ts.isDAG() {
+		return nil
+	}
+	return NewDepthFirstOrder(ts.g).reversePostQueue()
+}
+
+type SCC interface { //Strongly Connected Components
+	Connected(v, w int) bool
+	Count() int   //count of connected components
+	Id(v int) int // id of the component v belongs to
+}
+
+type normalSCC struct {
+	g          Digraph
+	components [][]int
+	marked     []bool
+	edgeTo     []int
+	onstack    []bool
+}
+
+func NewNormalSCC(g Digraph) SCC {
+	scc := &normalSCC{
+		g:       g,
+		marked:  make([]bool, g.V()),
+		edgeTo:  make([]int, g.V()),
+		onstack: make([]bool, g.V()),
+	}
+
+	for v := 0; v < g.V(); v++ {
+		scc.dfs(v)
+	}
+
+	return scc
+}
+
+func (scc *normalSCC) Connected(v, w int) bool {
+	return scc.Id(v) == scc.Id(w)
+}
+
+func (scc *normalSCC) Count() int {
+	return len(scc.components)
+}
+
+func (scc *normalSCC) Id(v int) int {
+	for i, c := range scc.components {
+		if utils.ContainsInt(c, v) {
+			return i
+		}
+	}
+	return v
+}
+
+func (scc *normalSCC) dfs(v int) {
+	scc.marked[v] = true
+	scc.onstack[v] = true
+	for _, w := range scc.g.Adj(v) {
+		current := w.(int)
+		scc.edgeTo[current] = v
+		if !scc.marked[current] {
+			scc.dfs(current)
+		} else if scc.onstack[current] {
+			var coms []int
+			coms = append(coms, current)
+			for x := v; x != current; x = scc.edgeTo[x] {
+				coms = append(coms, x)
+			}
+			scc.merge(coms)
+		}
+	}
+	scc.onstack[v] = false
+}
+
+func (scc *normalSCC) merge(coms []int) {
+	for _, c := range coms {
+		for i, cc := range scc.components {
+			if utils.ContainsInt(cc, c) {
+				scc.components[i] = append(scc.components[i], coms...)
+				return
+			}
+		}
+	}
+	scc.components = append(scc.components, coms)
 }
--- structure/digraph_test.go
+++ structure/digraph_test.go
@@ -19,12 +19,57 @@ func TestDiAdjGraph(t *testing.T) {
 }
 
 func TestNewDiAdjGraph(t *testing.T) {
-	g := NewDiAdjGraph(5, []edge{
-		{0, 1}, {2, 3}, {0, 3}, {4, 0}, {1, 4},
+	g := NewDiAdjGraph(7, []edge{
+		{0, 1}, {2, 3}, {0, 3}, {4, 0}, {1, 4}, {5, 6},
 	})
-	dfs := NewDirectedDfs(g, []int{0})
+	dfs := NewDirectedDfs(g, []int{0, 5})
 	assert.True(t, dfs.isMarked(1))
 	assert.True(t, dfs.isMarked(3))
 	assert.True(t, dfs.isMarked(4))
 	assert.False(t, dfs.isMarked(2))
+	assert.True(t, dfs.isMarked(6))
+}
+
+func TestNewDirectedCycleDetector(t *testing.T) {
+	g := NewDiAdjGraph(7, []edge{
+		{0, 1}, {2, 3}, {0, 3}, {4, 0}, {1, 4}, {5, 6},
+	})
+	dfs := NewDirectedCycleDetector(g)
+	assert.True(t, dfs.HasCycle())
+
+	g = NewDiAdjGraph(4, []edge{
+		{0, 1}, {0, 2}, {1, 3}, {2, 3},
+	})
+	dfs = NewDirectedCycleDetector(g)
+	assert.False(t, dfs.HasCycle())
+}
+
+func Test_DepthFirstOrder(t *testing.T) {
+	g := NewDiAdjGraph(13, []edge{
+		{0, 5}, {0, 1}, {0, 6}, {2, 0}, {2, 3}, {3, 5}, {5, 4}, {6, 4}, {7, 6}, {8, 7}, {6, 9}, {9, 10}, {9, 11}, {11, 12}, {9, 12},
+	})
+
+	df := NewDepthFirstOrder(g)
+
+	fmt.Println(df.pre)
+	fmt.Println(df.post)
+	fmt.Println(df.reversePostQueue())
+}
+
+func TestNewTopologicalSorter(t *testing.T) {
+	g := NewDiAdjGraph(13, []edge{
+		{0, 5}, {0, 1}, {0, 6}, {2, 0}, {2, 3}, {3, 5}, {5, 4}, {6, 4}, {7, 6}, {8, 7}, {6, 9}, {9, 10}, {9, 11}, {11, 12}, {9, 12},
+	})
+	sorter := NewTopologicalSorter(g)
+	fmt.Println(sorter.order())
+}
+
+func TestNormalSCC(t *testing.T) {
+	g := NewDiAdjGraph(13, []edge{
+		{0, 1}, {1, 2}, {2, 0}, {1, 6}, {6, 0}, {2, 3}, {3, 4}, {4, 5}, {5, 3},
+	})
+	scc := NewNormalSCC(g)
+	assert.True(t, scc.Connected(2, 6))
+	assert.True(t, scc.Connected(3, 5))
+	assert.False(t, scc.Connected(2, 3))
 }
